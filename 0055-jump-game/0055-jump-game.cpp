b9cc7b858c684931d1ea240a2a16f70d49e6d660
Abhoy Sarkar <90952001+yourflickbong@users.noreply.github.com>
2023-06-24 12:17:53 +0530

    Time: 81 ms (36.54%), Space: 48.4 MB (73.96%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int dest = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(i > dest)
+                return false;
+            else
+                dest = max(dest, i + nums[i]);
+        }
+        return true;
+    }
+};
