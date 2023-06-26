247bcdd772f9bf07724c18563c05ebcecebc932a
Abhoy Sarkar <90952001+yourflickbong@users.noreply.github.com>
2023-06-26 12:03:34 +0530

    Time: 30 ms (26.92%), Space: 17.7 MB (48.58%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> ans(ratings.size(),1);
+        int candies = 0;
+        for(int i = 1; i < ratings.size(); i++)
+        {
+            if(ratings[i] > ratings[i-1])
+                ans[i] = ans[i-1] + 1;
+        }
+        for(int i = ratings.size()-2; i >= 0; i--)
+        {
+            if(ratings[i]> ratings[i+1])
+            {
+                if(ans[i] <= ans[i+1])
+                    ans[i] = ans[i+1] + 1;
+            }
+                
+        }
+        for(int i = 0; i < ans.size(); i++)
+        {
+            candies += ans[i];
+        }
+        
+        return candies;
+    }
+};
