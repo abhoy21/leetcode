ca8977aff71cc150bd26306181540e817c761c6a
Abhoy Sarkar <90952001+yourflickbong@users.noreply.github.com>
2023-06-26 12:07:01 +0530

    Time: 30 ms (26.92%), Space: 17.7 MB (48.58%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -10,11 +10,8 @@ public:
         }
         for(int i = ratings.size()-2; i >= 0; i--)
         {
-            if(ratings[i]> ratings[i+1])
-            {
-                if(ans[i] <= ans[i+1])
-                    ans[i] = ans[i+1] + 1;
-            }
+            if(ratings[i]> ratings[i+1] && ans[i] <= ans[i+1])
+                ans[i] = ans[i+1] + 1;
                 
         }
         for(int i = 0; i < ans.size(); i++)
