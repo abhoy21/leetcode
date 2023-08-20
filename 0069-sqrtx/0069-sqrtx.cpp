e21dada938cf42194fa5187e35f7ab0e20e2b64c
Abhoy Sarkar <90952001+yourflickbong@users.noreply.github.com>
2023-08-20 19:43:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (43.10%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long l = 0;
+        long long r = x;
+        
+        while(l<=r)
+        {
+            long long mid = l + (r-l)/2;
+            
+            if((mid * mid) == x)
+            {
+                return mid;
+            }
+            
+            else if((mid * mid) > x)
+            {
+                r = mid - 1;
+            }
+                
+            else
+            {
+                l = mid + 1;
+            }
+                
+        }
+        
+        return r;
+    }
+};
