6891b0a04cfa90bc57e25f80cf7fa9f5756dc007
Abhoy Sarkar <90952001+yourflickbong@users.noreply.github.com>
2023-08-22 13:04:19 +0530

    Time: 7 ms (79.41%), Space: 7.4 MB (47.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        bool flag = 0;
-        vector<int> freq(26, 0);
-        if (s.length() != t.length()) {
-            return false;
-        }
-        for(int i = 0;i<s.length();i++)
+        vector<int> freq(26,0);
+        
+        if(s.length() != t.length())
+            return 0;
+        
+        for(int i = 0; i < s.length(); i++)
         {
-            freq[s[i]- 'a']++;
-            freq[t[i] - 'a']--;
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
         }
-        for(int i = 0; i< freq.size(); i++ )
+        
+        for(int i = 0; i < freq.size(); i++)
         {
             if(freq[i] != 0)
-            {
-                 return 0;
-                break;
-            }
-               
-
+                return 0;
         }
+        
+        
         return 1;
+        
     }
 };
