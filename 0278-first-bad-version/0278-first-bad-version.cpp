325b279ca9fd8d96c16174d644a3f76f67b1353d
Abhoy Sarkar <90952001+yourflickbong@users.noreply.github.com>
2023-08-27 11:58:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (46.40%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -16,6 +16,23 @@ public:
                 low=mid+1;
         }
         
-        return low;
+        return low;class Solution {
+public:
+    int firstBadVersion(int n) {
+        if(n==1)
+            return n;
+        int low = 1, high = n;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid))
+                 high = mid;
+             else
+                 low = mid + 1;
+        }
+        
+        return high;
+    }
+};
     }
 };
