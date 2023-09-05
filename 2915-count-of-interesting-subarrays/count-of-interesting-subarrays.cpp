5c93c7e0a0fcf42046b463a9bc4b8e068518068e
Abhoy Sarkar <90952001+yourflickbong@users.noreply.github.com>
2023-09-05 14:32:16 +0530

    Time: 169 ms (70.68%) | Memory: 112.4 MB (90.48%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -4,12 +4,9 @@ public:
         unordered_map<long long,long long> mp;
         long long ans = 0, prefix = 0, n = nums.size();
         mp[0]++;
-        // We will take Prefix Sum of nums[i]%mod=k
-        // For Example [3,1,9,6] [1,0,1,1] 
-        // prefix = [1,1,2,2] if its count==k till index i
 
         for(int i=0;i<n;i++) {
-            if(nums[i]%mod==k) // if my current element%m=k
+            if(nums[i]%mod==k)
                 prefix++;
             prefix%=mod;
             if(mp.find((prefix-k+mod)%mod)!=mp.end())
