7998b02d2f5e593ab2ff11a09354eaa1ebdbf49d
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2023-11-17 19:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (59.51%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    double exponent(double x, int n)
-    {
+    double exponent(double x, int n){
         if(n == 0)
             return 1;
-        double temp = exponent(x,n/2);
-        if(n%2 != 0)
+        double temp = exponent(x, n/2);
+        if(n % 2 != 0)
             return temp * temp * x;
-         return temp * temp; 
+        return temp * temp;
     }
     double myPow(double x, int n) {
         if(n < 0)
             return (1 / exponent(x,abs(n)));
-        return exponent(x,n);
+        return exponent(x, abs(n));
     }
 };
