4abc4e2b27afb2eb63fc448df5198332ddc583fc
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2023-11-23 20:33:42 +0530

    Time: 3 ms (39.48%) | Memory: 11.4 MB (9.01%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,57 +1,44 @@
 class Solution {
 public:
+
     string decodeString(string s) {
-        stack<char> stk;
-        stack<int> numStack; // Keep track of multipliers
-
-        string k = "";
-        string ans = "";
-
-        for (int i = 0; i < s.size(); i++) {
-            if (isdigit(s[i])) {
-                while (isdigit(s[i])) {
-                    k += s[i];
-                    i++;
-                }
-                numStack.push(stoi(k));
-                k = "";
-                i--; // Move the index back one step to handle the character after the number
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != ']') {
+                st.push(s[i]);
             }
-            else if (s[i] == '[') {
-                stk.push('[');
-            }
-            else if (s[i] == ']') {
-                string substring = "";
-                while (!stk.empty() && stk.top() != '[') {
-                    substring = stk.top() + substring;
-                    stk.pop();
-                }
-                stk.pop(); // Remove '[' from the stack
+            else{
+                string curr_str = "";
                 
-                int multiplier = numStack.top();
-                numStack.pop();
-
-                string repeatedSubstring = "";
-                for (int j = 0; j < multiplier; j++) {
-                    repeatedSubstring += substring;
+                while(st.top() != '['){
+                    curr_str = st.top() + curr_str ;
+                    st.pop();
                 }
-
-                // Push the repeated substring back onto the stack
-                for (char c : repeatedSubstring) {
-                    stk.push(c);
+                
+                st.pop();   // for '['
+                string number = "";
+                
+                // for calculating number
+                
+                while(!st.empty() && isdigit(st.top())){
+                    number = st.top() + number;
+                    st.pop();
+                }
+                int k_time = stoi(number);    // convert string to number
+                
+                while(k_time--){
+                    for(int p = 0; p < curr_str.size() ; p++)
+                        st.push(curr_str[p]);
                 }
             }
-            else {
-                stk.push(s[i]);
-            }
         }
-
-        // Construct the final answer by popping from the stack
-        while (!stk.empty()) {
-            ans = stk.top() + ans;
-            stk.pop();
+        
+        s = "";
+        while(!st.empty()){
+            s = st.top() + s;
+            st.pop();
         }
-
-        return ans;
+        return s;
+        
     }
 };
