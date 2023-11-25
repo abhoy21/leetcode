f7d69f96aaa10e10f6b92b5f52dd20ce8774335a
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2023-11-25 20:44:09 +0530

    Time: 8 ms (79.13%) | Memory: 18.4 MB (5.87%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int>st;
+        vector<int> ans;
+        stack<int> s;
         int n = asteroids.size();
-        for(int i = 0;i < n;i++){
-            int num = asteroids[i];
-            if(st.empty() || num > 0)
-                st.push(num);
-            else if(st.top() < 0)
-                st.push(num);
-            else{
-                while(!st.empty()){
-                    int a = st.top();
-                    if(st.top() < 0){
-                        st.push(num);
-                        break;
-                    }
-                    else if(st.top() > abs(num))
-                        break;
-                    else if(st.top() == abs(num)){
-                        st.pop();
-                        break;
-                    }
-                    else{
-                        st.pop();
-                        if(st.empty()){
-                            st.push(num);
-                            break;
-                        }
-                    }
+        
+        for (int i : asteroids) {
+            
+            
+            while(!s.empty() && s.top()>0 && i<0)
+            {
+                int sum = i+s.top();
+                if(sum<0)
+                {
+                 s.pop();   
+                }else if(sum>0)
+                {
+                    i = 0;
+                    break;
+                }else{
+                    i=0;
+                    s.pop();
+                    break;
                 }
             }
+
+            if(i!=0) {
+                s.push(i);
+            }
         }
-        vector<int>ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+
+        while (!s.empty()) {
+            ans.push_back(s.top());
+            s.pop();
         }
-        int low = 0;
-        int high = ans.size()-1;
-        while(low < high){
-            int temp = ans[low];
-            ans[low++] = ans[high];
-            ans[high--] = temp;
-        }
+        reverse(ans.begin(), ans.end());
+        
         return ans;
     }
 };
