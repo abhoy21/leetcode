57990b9dc8e07ce2d4b34c483bd3cc5d380bdaec
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2023-12-26 22:10:12 +0530

    Time: 9 ms (49.16%) | Memory: 21.8 MB (99.31%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,14 +11,14 @@
  */
 class Solution {
 public:
-    bool func(TreeNode* root, long int min, long int max){
+    bool f(TreeNode* root, long int min, long int max){
         if(root == nullptr)
             return true;
         if(root->val <= min || root->val >= max)
             return false;
-        return func(root->left, min, root->val) && func(root->right, root->val, max);
+        return f(root->left, min, root->val) && f(root->right, root->val, max);
     }
     bool isValidBST(TreeNode* root) {
-        return func(root, LONG_MIN, LONG_MAX);
+        return f(root, LONG_MIN, LONG_MAX);
     }
 };
