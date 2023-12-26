431ae60c0e90e164b3c29f8875cf4dd26bb241ff
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2023-12-26 12:44:15 +0530

    Time: 12 ms (53.48%) | Memory: 6.6 MB (99.78%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int func(int i, int j, string& s1, string& s2, vector<vector<int>>& dp){
-        if(i < 0)
-            return j + 1;
-        if(j < 0)
-            return i + 1;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(s1[i] == s2[j])
-            return dp[i][j] = func(i-1, j-1, s1, s2, dp);
-        return dp[i][j] = 1 + min(func(i-1, j, s1, s2, dp), 
-        min(func(i, j-1, s1, s2, dp), func(i-1,j-1, s1, s2, dp))); 
-    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return func(n-1, m-1, word1, word2, dp);
+        vector<int> prev(m+1, 0), cur(m+1, 0);
+
+        for(int j = 0; j <= m; j++)
+            prev[j] = j;
+        
+        for(int i = 1; i <= n; i++){
+            cur[0] = i;
+            for(int j = 1; j <= m; j++){
+
+                if(word1[i-1] == word2[j-1])
+                    cur[j] = prev[j-1];
+                else
+                    cur[j] = 1 + min(prev[j], min(cur[j-1], prev[j-1]));
+            }
+
+            prev = cur;
+        }
+
+        return prev[m];
     }
 };
