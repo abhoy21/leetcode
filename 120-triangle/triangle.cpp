e54a0300dad0c68015df05cdbbe6941f1d79ef75
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2024-01-01 21:27:38 +0530

    Time: 8 ms (30.89%) | Memory: 8.9 MB (81.42%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int func(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>> &dp) {
-        if(i == n-1)
-            return triangle[i][j];
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int down = triangle[i][j] + func(i+1, j, n,triangle, dp);
-        int right = triangle[i][j]+ func(i+1, j+1,n,triangle,dp);
-        return dp[i][j] = min(down, right);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> prev(n, 0), cur(n, 0);
         for(int i = 0; i < n; i++)
-            dp[n-1][i] = triangle[n-1][i];
+            prev[i] = triangle[n-1][i];
         for(int i = n-2; i >= 0; i--){
             for(int j = i; j >= 0; j--){
-                int up = triangle[i][j] + dp[i+1][j];
-                int diagonal = triangle[i][j]+ dp[i+1][j+1];
+                int up = triangle[i][j] + prev[j];
+                int diagonal = triangle[i][j]+ prev[j+1];
 
-                dp[i][j] = min(up, diagonal);
+                cur[j] = min(up, diagonal);
             }
+            prev = cur;
         }
 
-        return dp[0][0];
+        return prev[0];
     }
 };
