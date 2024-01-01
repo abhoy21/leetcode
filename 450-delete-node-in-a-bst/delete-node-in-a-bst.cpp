b1fd496b2ef8154e3efe51ddb2d76e3e258a6595
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2024-01-01 14:44:08 +0530

    Time: 46 ms (5.10%) | Memory: 33.1 MB (15.17%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -12,52 +12,46 @@
 class Solution {
 public:
     TreeNode* lastRightchild(TreeNode* root) {
-        if (!root->right)
+        if(!root->right)
             return root;
         return lastRightchild(root->right);
     }
-
-    TreeNode* helper(TreeNode* root) {
-        if (!root->left)
+    TreeNode* helper(TreeNode* root){
+        if(!root->left)
             return root->right;
-        else if (!root->right)
+        else if(!root->right)
             return root->left;
         TreeNode* rightchild = root->right;
         TreeNode* lastRC = lastRightchild(root->left);
         lastRC->right = rightchild;
-        return root->left;
-    }
 
+        return root->left;
+
+    }
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (!root)
+        if(!root)
             return nullptr;
-        if (root->val == key)
+        if(root->val == key)
             return helper(root);
         TreeNode* node = root;
-        TreeNode* parent = nullptr;
-
-        // Find the node to be deleted and its parent
-        while (root && root->val != key) {
-            parent = root;
-            if (root->val > key)
-                root = root->left;
-            else
-                root = root->right;
+        while(root) {
+            if(root->val > key){
+                if(root->left && root->left->val == key) {
+                    root->left = helper(root->left);
+                    break;
+                }
+                else
+                    root = root->left;
+            }
+            else{
+                if(root->right && root->right->val == key){
+                    root->right = helper(root->right);
+                    break;
+                }
+                else
+                    root= root->right;
+            }
         }
-
-        if (!root)
-            return node; // Node with key not found
-
-        if (!parent) {
-            // The node to be deleted is the root
-            return helper(root);
-        }
-
-        // Update the parent's pointer to the deleted node
-        if (parent->left == root)
-            parent->left = helper(root);
-        else
-            parent->right = helper(root);
 
         return node;
     }
