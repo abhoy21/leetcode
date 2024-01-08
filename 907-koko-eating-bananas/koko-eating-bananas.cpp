7e59889a9c154c3519de5218cea2ab0509509e7d
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2024-01-08 21:40:22 +0530

    Time: 40 ms (51.14%) | Memory: 19.3 MB (36.82%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    long long hrsfunc(vector<int>& piles, int bhr){
-        long long totalhrs = 0;
-        for(int i = 0; i < piles.size(); i++){
-            int temp = ceil(piles[i] / (double)bhr);
-            totalhrs += temp;
+    long long hrsfunc(vector<int> &piles, int h) {
+        long long total = 0;
+        for(int i = 0; i < piles.size(); i++) {
+            int temp = ceil((double)piles[i] / (double)h);
+            total += temp;
         }
 
-        return totalhrs;
-    }
-    int findMaxElement(vector<int>& piles) {
-        int maxElement = piles[0];
-
-        for (int i = 1; i < piles.size(); ++i) {
-            if (piles[i] > maxElement) {
-                maxElement = piles[i];
-            }
+        return total;
+    } 
+    int maxele(vector<int> &piles) {
+        int maxi = piles[0];
+        for(int i = 1; i < piles.size(); i++) {
+            if(maxi < piles[i])
+                maxi = piles[i];
         }
 
-        return maxElement;
+        return maxi;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1, high = findMaxElement(piles);
-        int ans = -1;
+        int low = 1, high = maxele(piles);
 
-        while(low <= high){
+        while(low <= high) {
             int mid = low + (high - low) / 2;
-            
             long long hrs = hrsfunc(piles, mid);
-
-            if(hrs <= h){
-                ans = mid;
+            if(hrs <= h) 
                 high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
+            else
+                low = mid + 1; 
         }
 
-        return ans;
+        return low;
     }
 };
