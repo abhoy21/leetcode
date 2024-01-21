e67613754f43e3d1ac6383447c1d047e8a966c8b
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2024-01-21 09:36:43 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,9 +3,8 @@ public:
     int rob(vector<int>& nums) {
         int rob1 = 0, rob2 = 0;
 
-        for(int n : nums)
-        {
-            int temp = max(n+rob1, rob2);
+        for(int i : nums){
+            int temp = max(i + rob1, rob2);
             rob1 = rob2;
             rob2 = temp;
         }
