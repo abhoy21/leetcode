a79a5679ff2fcc18037f713a9a63181efae7cf0d
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2024-02-04 19:07:29 +0530

    Time: 4 ms (91.16%) | Memory: 9.3 MB (22.77%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,53 +1,27 @@
 class Solution {
-public:
-    string minWindow(string s, string t) {
-        if (s.length() < t.length())
-            return "";
+ public:
+  string minWindow(string s, string t) {
+    vector<int> count(128);
+    int required = t.length();
+    int bestLeft = -1;
+    int minLength = s.length() + 1;
 
-        unordered_map<char, int> mp_t;
+    for (const char c : t)
+      ++count[c];
 
-        for (char ch : t)
-            mp_t[ch]++;
-
-        int l = 0, r = 0, c = t.length(), size = INT_MAX;
-        int min_l = 0; // Track the starting position of the minimum window
-
-        while (r < s.length()) {
-            char ele = s[r];
-
-            if (mp_t.find(ele) != mp_t.end()) {
-                mp_t[ele]--;
-                if (mp_t[ele] >= 0) {
-                    c--;
-                }
-            }
-
-            while (c == 0) {
-                if (r - l + 1 < size) {
-                    size = r - l + 1;
-                    min_l = l; // Update the minimum window's starting position
-                }
-
-                char left_char = s[l];
-                if (mp_t.find(left_char) != mp_t.end()) {
-                    mp_t[left_char]++;
-                    if (mp_t[left_char] > 0) {
-                        c++;
-                    }
-                }
-
-                l++;
-            }
-
-            r++;
+    for (int l = 0, r = 0; r < s.length(); ++r) {
+      if (--count[s[r]] >= 0)
+        --required;
+      while (required == 0) {
+        if (r - l + 1 < minLength) {
+          bestLeft = l;
+          minLength = r - l + 1;
         }
-
-        return size == INT_MAX ? "" : s.substr(min_l, size);
+        if (++count[s[l++]] > 0)
+          ++required;
+      }
     }
+
+    return bestLeft == -1 ? "" : s.substr(bestLeft, minLength);
+  }
 };
-
-
-
-
-
-
