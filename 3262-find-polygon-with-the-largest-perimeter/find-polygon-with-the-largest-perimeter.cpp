05d54299113032db597f8fafa5139c172f6c0b92
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2024-02-15 15:24:34 +0530

    Time: 144 ms (59.76%) | Memory: 95.1 MB (6.44%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.cpp
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.cpp
@@ -4,10 +4,11 @@ public:
         sort(nums.begin(), nums.end());
         vector<long long> prefix;
         long long sum = 0;
-        for(auto n : nums){
+        for(int n : nums){
             prefix.push_back(sum);
             sum += n;
         }
+
         for(int i = nums.size()-1; i >= 2; i--){
             if(prefix[i] > nums[i])
                 return prefix[i] + (long long)nums[i];
