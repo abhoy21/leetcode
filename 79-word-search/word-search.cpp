f6cb508d3338334d1e18f10f71e520e7d967aa86
Abhoy Sarkar <90952001+abhoy21@users.noreply.github.com>
2024-04-03 09:19:44 +0530

    Time: 993 ms (38.41%) | Memory: 11.6 MB (49.58%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    bool check(int i, int j, int m, int n, vector<vector<char>>& board, string word, int k)
-    {
-      if(k == word.size())
-        return true;
-      if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[k])
-        return false;
-      char ch = board[i][j];
-      board[i][j] = '1';
-      bool check1 = check(i+1, j, m, n, board, word, k+1);
-      bool check2 = check(i, j+1, m, n, board, word, k+1);
-      bool check3 = check(i-1, j, m, n, board, word, k+1);
-      bool check4 = check(i, j-1, m, n, board, word, k+1);
-      board[i][j] = ch;
+    bool func(vector<vector<char>>& board, string word, int i, int j, int n, int m, int k){
+        if(k == word.size()) return true;
+        if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[k]) return false;
+        char ch = board[i][j];
+        board[i][j] = '1';
 
-      return check1 || check2 || check3 || check4;
+        bool up = func(board, word, i+1, j, n, m, k+1);
+        bool down = func(board, word, i-1, j, n, m, k+1);
+        bool left = func(board, word, i, j-1, n, m, k+1);
+        bool right = func(board, word, i, j+1, n, m, k+1);
+
+        board[i][j] = ch;
+
+        return up || down || left || right;
+
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
 
-        for(int i = 0; i < n; i++)
-        {
-          for(int j = 0; j < m; j++)
-          {
-            if(check(i, j, m, n, board, word, 0)) return true;
-          }
-        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(func(board, word, i, j, n, m, 0)) return true;
+            }
+        }        
 
         return false;
     }
